046a1efe4c624962dd8ad8153bad034f435f2cd7
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2020-06-15 18:08:07 +0100

    users

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/users.controller")
+const auth = require("../middleware/auth.middleware")
 
-router.get("/", controller.getUsers)
-router.post("/", controller.addUser)
-router.delete("/:id", controller.deleteUser)
-router.put("/:id", controller.editUser)
+router.get("/", auth, controller.getUsers)
+router.post("/", auth, controller.addUser)
+router.delete("/:id", auth, controller.deleteUser)
+router.put("/:id", auth, controller.editUser)
+
 module.exports = app => app.use("/users", router)
