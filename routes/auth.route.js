02806df09971249e27d2dcbf2d0d3d62082eb88c
Guilherme  Costa <guilhermecosta@AirdeGuilherme.lan>
2021-01-10 15:29:15 +0000

    auth complete

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,6 +3,5 @@ const router = express.Router()
 const controller = require("../controllers/auth.controller")
 
 router.post("/sign-in", controller.signIn)
-router.post("/sign-up", controller.signUp)
 
 module.exports = app => app.use("/auth", router)
